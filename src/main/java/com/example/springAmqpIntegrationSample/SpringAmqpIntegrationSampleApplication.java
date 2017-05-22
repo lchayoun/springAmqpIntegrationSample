5f82098ebcd38c775199b95e932297635cc6ebd6
lchayoun <liel.chayoun@intel.com>
2017-05-22 16:13:39 +0300

    initial commit

--- src/main/java/com/example/springAmqpIntegrationSample/SpringAmqpIntegrationSampleApplication.java
+++ src/main/java/com/example/springAmqpIntegrationSample/SpringAmqpIntegrationSampleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableBinding({ExampleProcessor.class})
 public class SpringAmqpIntegrationSampleApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/springAmqpIntegrationSample/AbstractSpringAmqpIntegrationSampleApplicationTests.java
+++ src/test/java/com/example/springAmqpIntegrationSample/AbstractSpringAmqpIntegrationSampleApplicationTests.java
@@ -4,12 +4,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @AutoConfigureTestDatabase
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AbstractSpringAmqpIntegrationSampleApplicationTests {
-    @MockBean protected ExampleProcessor exampleProcessor;
 }
